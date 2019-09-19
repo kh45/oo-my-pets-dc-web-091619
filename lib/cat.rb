169824ce4728dcc27d387b9c22495eb9326030e9
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-19 12:33:40 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
-  # code goes here
+  
+  attr_reader :name
+  attr_accessor :owner, :mood
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
 end
