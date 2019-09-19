4c09cd75dbbcd2c69d7dd3f7e768ab5e59c05e6a
industrious-modal-4420 <khaledhassan45@gmail.com>
2019-09-19 12:38:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,10 +3,16 @@ class Dog
   attr_reader :name
   attr_accessor :owner, :mood
   
+  @@all = []
+  
   def initialize(name, owner, mood = "nervous")
     @name = name
     @owner = owner
     @mood = mood
+  end
+  
+  def self.all
+    @@all
   end
   
 end
